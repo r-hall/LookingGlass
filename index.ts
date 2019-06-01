26d2b95b4817944d043d03260ff4562cd553cc0e
r-hall <ryanhall2015@gmail.com>
2019-06-01 14:29:33 -0700

    Added endpoints for fetching tweets by a given user and user profile.

--- index.ts
+++ index.ts
@@ -2,9 +2,12 @@ import express = require('express');
 import bodyParser = require('body-parser');
 import morgan = require('morgan');
 import cors = require('cors');
-const Users = require('./db.ts').Users;
-const Friends = require('./db.ts').Friends;
-const TwitterClient = require('./TwitterClient.ts');
+// Mongoose models
+const Users = require('./db.js').Users;
+const Friends = require('./db.js').Friends;
+const Lists = require('./db.js').Lists;
+
+const TwitterClient = require('./TwitterClient.js');
 const port = process.env.PORT || 3001;
 
 var app = express();
@@ -18,7 +21,7 @@ app.use(cors());
 app.get('/health', (req, res) => {
   res.writeHead(200);
   res.end('healthy');
-})
+});
 
 app.get('/search', async (req, res) => {
     try {
@@ -79,16 +82,90 @@ app.get('/friends', async (req, res) => {
     let userId = req.query.userId;
     let viewerId = req.query.viewerId;
     let viewer = await Users.findOne({id: viewerId});
-    let client = new TwitterClient(viewer)
+    let client = new TwitterClient(viewer);
     let friends = await client.getFriends(userId, viewerId);
-    res.writeHead(200)
-    res.end(JSON.stringify(friends))
+    res.writeHead(200);
+    res.end(JSON.stringify(friends));
   } catch(err) {
-    console.log('ERROR in friendsAPI', err);
+    console.log('ERROR in get /friends', err);
     res.writeHead(404);
     res.end(err);
-  }
-})
+  };
+});
+
+app.get('/tweets', async (req, res) => {
+  try {
+    let userId = req.query.userId;
+    let user = await Users.findOne({id: userId});
+    let client = new TwitterClient(user);
+    let tweets = await client.getTweets(userId);
+    res.writeHead(200);
+    res.end(JSON.stringify(tweets));
+  } catch(err) {
+    console.log('ERROR in /tweets', err);
+    res.writeHead(404);
+    res.end(err);
+  };
+});
+
+app.get('/user', async (req, res) => {
+  try {
+    let userId = req.query.userId;
+    let viewerId = req.query.viewerId;
+    let viewer = await Users.findOne({id: viewerId});
+    let client = new TwitterClient(viewer);
+    let user = await client.getUser(client, userId);
+    res.writeHead(200);
+    res.end(JSON.stringify(user));
+  } catch(err) {
+    console.log('ERROR in /user', err);
+    res.writeHead(404);
+    res.end(err);
+  };
+});
+
+// app.get('/timeline', async (req, res) => {
+//   try {
+//     let userId = req.query.userId;
+//     let name = req.query.userScreenName;
+//     let viewerId = req.query.viewerId;
+//     let user = await Users.findOne({id: userId});
+//     let client = new TwitterClient(user);
+//     if (user && user.twitterTokenKey) {
+//       let timeline = await client.getOwnTimeline(client, userId);
+//       res.writeHead(200);
+//       res.end(JSON.stringify(timeline));
+//     } else {
+//       let list = await Lists.findOne({id: name});
+//       // TODO: Error handling -- user deletion of list on Twitter.
+//       if (list && list.done) {
+//         let timeline = await client.getListTimeline(list);
+//         res.writeHead(200);
+//         res.end(JSON.stringify(timeline));
+//       } else {
+//         client.requestList(name, userId, viewerId);
+//         let tokenInfo = await TokenTracker.findOne({id: 1});
+//         let currentToken = tokenInfo.currentToken;
+//         let totalTokens = tokenInfo.totalTokens;
+//         let updateObject = {};
+//         let newCurrentToken = currentToken === totalTokens ? 1 : currentToken + 1;
+//         updateObject['currentToken'] = newCurrentToken;
+//         let query = {'id': 1};
+//         TokenTracker.findOneAndUpdate(query, updateObject);
+//         let token = await Tokens.findOne({id: currentToken});
+//         let tokenKey = token.twitterTokenKey;
+//         let tokenSecret = token.twitterTokenSecret;
+//         let timeline = await getOtherTimeline(tokenKey, tokenSecret, userId, sqs);
+//         res.writeHead(200);
+//         res.end(timeline);
+//       }
+//     }
+//   } catch(err) {
+//     console.log('error in /timeline/:userId/:userScreenName/:viewerId', err);
+//     res.writeHead(404);
+//     res.end(err);
+//   }
+// })
 
 app.listen(port, () => {
 	console.log(`listening on port ${port}`);
