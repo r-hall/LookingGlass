a698112da4cfdf3ed6884b099b72ec796ad89871
r-hall <ryanhall2015@gmail.com>
2019-06-01 14:26:18 -0700

    Added methods getLikesBatch, getTweets, getUser, getOwnTimeline, getListTimeline, getListsByOwner.

--- TwitterClient.ts
+++ TwitterClient.ts
@@ -1,7 +1,15 @@
+// Twitter library
 const Twitter = require('twitter');
+// Twitter application authentication credentials
 const authAPI = require('./config/twitter.js');
+// Mongoose models
 const Friends = require('./db.js').Friends;
 const Users = require('./db.js').Users;
+// const Lists = require('./db.js').Lists;
+// Utils
+const calculateNextDate = require('./utils/calculateNextDate.js').calculateNextDate;
+const boundLikes = require('./utils/boundInput.js').boundLikes;
+const computeMinId = require('./utils/computeMinId.js').computeMinId;
 
 const refreshFriendsQueue = require('./config/awsURLs.js').refreshFriendsQueue;
 
@@ -46,18 +54,53 @@ class TwitterClient {
         });
     };
     
-    // Return 200 most recent likes of a given user.
-    getLikes(userId: string) {
+    // Return 200 most recent likes older than a given tweet of a given user.
+    getLikes(userId: string, maxId?: string, minId?: string) {
         const endpoint = 'favorites/list';
-        const params = {
+        let params: any = {
             'user_id': userId,
             'count': 200,
             'include_entities': true,
             'tweet_mode': 'extended'
         };
+        if (maxId) { params['max_id'] = maxId; };
+        if (minId) { params['since_id'] = minId; };
         return new Promise( async (resolve, reject) => {
             try {
                 let likes = await this.client.get(endpoint, params);
+                resolve(likes);
+            } catch(error) {
+                console.log('error in twitterFetchLikes', error)
+                reject(error);
+            };
+        });
+    };
+
+    // Return given number of likes of a given user.
+    getLikesBatch(userId: string, numLikes: number) {
+        const maxLikesPerBatch = 200;
+        numLikes = boundLikes(numLikes);
+        const numBatches = Math.ceil(numLikes / maxLikesPerBatch)
+        const endpoint = 'favorites/list';
+        return new Promise( async (resolve, reject) => {
+            try {
+                let likes: any = [];
+                let count = numLikes > maxLikesPerBatch ? maxLikesPerBatch : numLikes;
+                let params: any = {
+                    'user_id': userId,
+                    'count': count,
+                    'include_entities': true,
+                    'tweet_mode': 'extended'
+                };
+                for (let i = 0; i < numBatches; i++) {
+                    let batchLikes = await this.client.get(endpoint, params);
+                    likes.push(...batchLikes);
+                    numLikes -= count;
+                    let minId = computeMinId(batchLikes);
+                    params['max_id'] = minId;
+                    count = numLikes > maxLikesPerBatch ? maxLikesPerBatch : numLikes;
+                    params['count'] = count;
+                };
                 resolve(likes);
             } catch(error) {
                 console.log('error in twitterFetchLikes', error)
@@ -108,7 +151,7 @@ class TwitterClient {
         return new Promise( async (resolve, reject) => {
             try {
                 let friend = await addFriendTwitter(friendId);
-                addFriendDB(friend, viewerId, oldFriends);
+                // addFriendDB(friend, viewerId, oldFriends);
                 resolve(friend);
             } catch(err) {
                 console.log('ERROR in addFriend', err);
@@ -116,7 +159,7 @@ class TwitterClient {
         });
     };
 
-    sendAWSMessageRequest = (message: string, queueURL: string) => {
+    sendAWSMessageRequest(message: string, queueURL: string) {
         const params = {
                 DelaySeconds: 10,
                 MessageBody: message,
@@ -127,12 +170,12 @@ class TwitterClient {
                 console.log("Error in refresh-friends", err);
             } else {
                 console.log("Success in refresh-friends", data.MessageId);
-            }
+            };
         });
-    }
+    };
 
     // Get all friend ids for a given Twitter user.
-    fetchTwitterFriendIds = (userId: string, results: any) => {
+    _fetchTwitterFriendIds(userId: string) {
         let endpoint = 'friends/ids'; 
         let params = {
             'user_id': userId,
@@ -142,27 +185,25 @@ class TwitterClient {
         };
         return new Promise( async (resolve, reject) => {
           try {
-            let response = await this.client.get(endpoint, params);
-            // add ids to friends for that user
-            for (let i = 0; i < response.ids.length; i++) {
-                results.push(response.ids[i])
+            let response;
+            let results = [];
+            while (params.cursor !== 0) {
+                response = await this.client.get(endpoint, params);
+                for (let i = 0; i < response.ids.length; i++) {
+                    results.push(response.ids[i]);
+                };
+                params.cursor = response.next_cursor;
             }
-            if (response.next_cursor_str !== "0") {
-              params.cursor = response.next_cursor_str;
-              results = results.concat(await this.fetchTwitterFriendIds(userId, []));
-              resolve(results);
-            } else {
-              resolve(results);
-            } 
+            resolve(results);
           } catch(error) {
             console.log('error in fetchTwitterFriendIds', error);
             reject(error);
-          }
+          };
         });
-      }
+      };
       
       // Get fully-hydrated user objects for a given list of ids.
-      fetchTwitterUserObjects(ids: any, index: number, results: any) {
+      _fetchTwitterUserObjects(ids: any, index: number, results: any) {
           const endpoint = 'users/lookup';
           const maxObjects = 100;
           return new Promise( async (resolve, reject) => {
@@ -170,65 +211,193 @@ class TwitterClient {
                 let idsInput = ids.slice(index, index + maxObjects).join(',');
                 let params = {
                     'user_id': idsInput
-                }
+                };
                 let response = await this.client.post(endpoint, params);
                 // add ids to friends for that user
                 for (let i = 0; i < response.length; i++) {
                     results.push(response[i])
-                }
+                };
                 // make API call again if not all users retrieved
                 if (index + maxObjects < ids.length) {
                     index += maxObjects;
-                    results = results.concat(await this.fetchTwitterUserObjects(ids, index, []));
+                    results = results.concat(await this._fetchTwitterUserObjects(ids, index, []));
                     resolve(results);
                 } else {
                   resolve(results);
-                } 
+                };
               } catch(error) {
                 console.log('error in fetchTwitterUserObjects', error)
                 reject(error);
-              }
+              };
           });
-      }
+      };
 
     // Return friends of a given user.
     getFriends(userId: string, viewerId: string) {
         return new Promise( async (resolve, reject) => {
             try {
-                let message = viewerId + '.' + userId;
-                let user = await Friends.findOne({id: userId});
-                if (user && user.friends.length) {
-                    let friends = user.friends;
-                    let refreshedFriendsDate = +user.refreshedFriendsDate;
-                    let currentDate = +new Date();
-                    let hours = Number(Math.abs(refreshedFriendsDate - currentDate) / 36e5);
-                    if (hours > 24) {
-                        // this.sendAWSMessageRequest(message, refreshFriendsQueue);
-                        resolve([friends, false]);
-                    } else {
-                        resolve([friends, true]);
-                    }
-                } else {
-                    let viewer = await Users.findOne({id: viewerId})
+                // let message = viewerId + '.' + userId;
+                // let user = await Friends.findOne({id: userId});
+                // if (user && user.friends.length) {
+                //     let friends = user.friends;
+                //     let refreshedFriendsDate = +user.refreshedFriendsDate;
+                //     let currentDate = +new Date();
+                //     let hours = Number(Math.abs(refreshedFriendsDate - currentDate) / 36e5);
+                //     if (hours > 24) {
+                //         // this.sendAWSMessageRequest(message, refreshFriendsQueue);
+                //         resolve(friends);
+                //     } else {
+                //         resolve(friends);
+                //     };
+                // } else {
                     // this.sendAWSMessageRequest(message, refreshFriendsQueue);
-                    let friend_ids = await this.fetchTwitterFriendIds(userId, []);
-                    let friends = await this.fetchTwitterUserObjects(friend_ids, 0, []);
-                    resolve([friends, false]);
-                }
+                    let friend_ids = await this._fetchTwitterFriendIds(userId);
+                    let friends = await this._fetchTwitterUserObjects(friend_ids, 0, []);
+                    resolve(friends);
+                // };
             } catch(error) {
                 console.log('error in getFriends');
-                reject(error)
+                reject(error);
+            };
+        });
+    };
+    
+    // Return 200 most recent tweets, excluding replies, older than given tweet of a given user.
+    getTweets(userId: string, maxId?: string, minId?: string) {
+        return new Promise( async (resolve, reject) => {
+            try {
+                let endpoint = 'statuses/user_timeline'; 
+                let params: any = {
+                    'user_id': userId,
+                    'count': 200,
+                    'tweet_mode': 'extended',
+                    'exclude_replies': true,
+                    'include_rts': false
+                };
+                if (maxId) { params['max_id'] = maxId; };
+                if (minId) { params['since_id'] = minId; };
+                let tweets = await this.client.get(endpoint, params);
+                resolve(tweets);
+            } catch(error) {
+                console.log('ERROR in getTweets', error);
+                reject(error);
+            };
+        });
+    };
+
+    // Get user object for given user.
+    getUser(userId: string) {
+        return new Promise( async (resolve, reject) => {
+            try {
+                let endpoint = 'users/show'; 
+                let params = {
+                    'user_id': userId
+                };
+                let user = await this.client.get(endpoint, params);
+                resolve(user);
+            } catch(err) {
+                console.log('ERROR in getUserObject', err);
+                reject(err);
+            };
+        });
+    };
+    
+    // Get first 200 tweets in user's timeline.
+    getOwnTimeline(userId: string, maxId?: string, minId?: string) {
+        return new Promise( async (resolve, reject) => {
+            try {
+                // TODO: Handle rate limits.
+                let endpoint = 'statuses/home_timeline'; 
+                let params: any = {
+                    'count': 200,
+                    'tweet_mode': 'extended'
+                };
+                if (maxId) { params['max_id'] = maxId; };
+                if (minId) { params['since_id'] = minId; };
+                let timeline = await this.client.get(endpoint, params);
+                resolve(timeline);
+            } catch(error) {
+                console.log('ERROR in getOwnTimeline', error);
+                reject(error);
             }
         })
     }
+
+    // Get 200 tweets from timeline given list.
+    getListTimeline(list: any) {
+        // TODO: Make list interface.
+        return new Promise( async (resolve, reject) => {
+            try {
+                const endpoint = 'lists/statuses';
+                let params = {
+                    'slug': list.slug,
+                    'owner_screen_name': list.owner_screen_name,
+                    'count': 200
+                }
+                let timeline = await this.client.get(endpoint, params);
+                resolve(timeline);
+            } catch(err) {
+                console.log('error in getListTimeline', err);
+                reject(err);
+            }
+        })
+    }
+
+    getListsByOwner(user_id: string) {
+        return new Promise( async (resolve, reject) => {
+            try {
+                const endpoint = 'lists/list';
+                let params = {
+                    'user_id': user_id
+                }
+                let lists = await this.client.get(endpoint, params);
+                resolve(lists);
+            } catch(err) {
+                console.log('error in getListTimeline', err);
+                reject(err);
+            }
+        })
+    }
+
+
+    // async requestList(name: string, userId: string, viewerId: string) {
+    //     try {
+    //         // Ensure the rate limit for GET lists/statuses is not exceeded.
+    //         const maxLists = 50;
+    //         // Add a safe amount to list each day, assuming some members are added by the user outside of this app.
+    //         const maxDailyInsertions = 400;
+    //         let viewer = await Users.findOne({id: viewerId});
+    //         // TODO: What if they aren't? Need to find a suitable user to build the list.
+    //         if (viewer.numberOfLists < maxLists) {
+    //             let params = {
+    //                 'user_id': userId,
+    //                 'count': 5000,
+    //                 'cursor': -1,
+    //                 'stringify_ids': true
+    //             };
+    //             let friends: any = await this._fetchTwitterFriendIds(userId, []);
+    //             const batches = Math.ceil(friends.length / maxDailyInsertions);
+    //             let currentDate = new Date();
+    //             let dateUsed = currentDate > viewer.lastListBuild ? currentDate : viewer.lastListBuild;
+    //             let endBuildDate = calculateNextDate(dateUsed, batches);
+    //             let listQuery = { 'id': name };
+    //             let listUpdateObject = {
+    //                 'id': name,
+    //                 'done': false,
+    //                 'batches': batches,
+    //                 'friends': friends,
+    //                 'currentBatch': 0,
+    //             }
+    //             Lists.findOneAndUpdate(listQuery, listUpdateObject, {upsert: true});
+    //             let userQuery = { 'id': viewerId };
+    //             let userUpdateObject = { 'lastListBuild': endBuildDate };
+    //             Users.findOneAndUpdate(userQuery, userUpdateObject);
+    //             scheduleJobs(sqs, name, viewerId, dateUsed, batches); // schedule messages to lambda function that will build list
+    //         }
+    //     } catch(err) {
+    //         console.log('error in requestList', err);
+    //     }
+    // }
 };
 
-// module.exports = TwitterClient;
-const mainFunc = async () => {
-    let client = new TwitterClient();
-    const userId = '1129503751901831168';
-    const friendId = '12';
-    const friends: any = await client.addFriend(friendId, userId);
-    console.log(friends[0]);
-}
-mainFunc();
+module.exports = TwitterClient;
